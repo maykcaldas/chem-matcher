a444b4e0f9382a5722328673a51d94a1f0c550f5
maykcaldas <maykcaldas@gmail.com>
2024-03-19 10:34:43 -0400

    Updated utils scripts

--- data/get_s2orc.py
+++ data/get_s2orc.py
@@ -1,10 +1,11 @@
 import requests
 import os
 from dotenv import load_dotenv
+from tqdm import tqdm
 
 load_dotenv()
 
-base_url = "https://api.semanticscholar.org/datasets/v1/release/"
+base_url = "https://api.semanticscholar.org/datasets/v1/release"
 response = requests.get(base_url)
 if response.status_code == 200:
    release_id = response.json()[-1]
@@ -23,14 +24,15 @@ else:
     raise Exception(f"Failed to get dataset info: Status code {response.status_code}")
 
 for i, url in enumerate(response_data['files'], start=1):
+    if i > 10: break # don't want to download all 260GB
     filename = url.split('/')[-1].split('?')[0]  
     response = requests.get(url, stream=True)  
 
     if response.status_code == 200:
+        print(f"Downloading chunk{i}: {filename}")
         with open(f"chunk{i}.gz", 'wb') as file:
-            for chunk in response.iter_content(chunk_size=128):
+            for chunk in tqdm(response.iter_content(chunk_size=128)):
                 file.write(chunk)
         print(f"Downloaded chunk{i}: {filename}")
     else:
         print(f"Failed to download {filename}: Status code {response.status_code}")
-
--- data/run.sh
+++ data/run.sh
@@ -5,13 +5,13 @@ cargo build --release
 cp ../target/release/chem-matcher .
 
 
-# FILE_PATHS=$(find . -name "chunk*.gz" | sort | sed 's|\./||g')
-# ./chem-matcher  -c ../../../Data/CID-Synonym-filtered \
-#                 -f $FILE_PATHS \
-#                 -o output.csv
-
-# ./chem-matcher  -c ../../../Data/test_map2 \
-# ./chem-matcher  -c ../../../Data/CID-SMILES-Synonyms \
+FILE_PATHS=$(find . -name "chunk*.gz" | sort | sed 's|\./||g')
 ./chem-matcher  -c ../../../Data/CID-Synonym-filtered \
-                -f chunk1.gz \
+                -f $FILE_PATHS \
                 -o output.csv --stop 2000
+
+# ./chem-matcher  -c ../../../Data/test_map \
+# ./chem-matcher  -c ../../../Data/CID-SMILES-Synonyms \
+#                 -f chunk1.gz \
+#                 -o output.csv --stop 2000
+# ./chem-matcher  -c ../../../Data/CID-Synonym-filtered \
